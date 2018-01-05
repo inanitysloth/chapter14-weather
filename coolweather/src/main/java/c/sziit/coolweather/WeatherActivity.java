41d48588ef7d0edd44a28363aa1e836763e13e4e
64881623@qq.com <CYP_lh163>
2018-01-05 08:39:00 +0800

    4 2018.1.4日中午午完成Goson的数据解析，其中接受Gson数据的类来自于jason网站的自动生成

--- coolweather/src/main/java/c/sziit/coolweather/WeatherActivity.java
+++ coolweather/src/main/java/c/sziit/coolweather/WeatherActivity.java
@@ -135,6 +135,7 @@ public class WeatherActivity extends AppCompatActivity implements View.OnClickLi
         mWeatherInfoText.setText(weather.getNow().getCond().getTxt());
         //forecast.xml
         mForecastLayout.removeAllViews();
+        //未来几天
         for (Daily_forecast forecast:weather.getDaily_forecast())
         {
             View view = LayoutInflater.from(this).inflate(R.layout.forecast_item, mForecastLayout, false);
